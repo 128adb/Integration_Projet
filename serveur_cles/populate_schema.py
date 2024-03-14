3d4aa3543e6b557b5105e805a157a270f50ee053
StayZeen <84673211+StayZeen@users.noreply.github.com>
2024-03-14 14:27:35 +0100

    ajoute fake data

--- serveur_cles/populate_schema.py
+++ serveur_cles/populate_schema.py
@@ -3,16 +3,43 @@ import random
 import time
 import utile.data as data
 
-# valeurs de simulation
-fake_victims = []
+# valeurs de simulationa
+fake_victims = [
+    ['WORKSTATION', 'c:,e:,f:', 'PENDING', 108],
+    ['SERVEUR', 'c:,e:', 'PROTECTED', 23],
+    ['WORKSTATION', 'c:,f:', 'INITIALIZE', 0],
+    ['WORKSTATION', 'c:,f:,y:,z:', 'PROTECTED', 108]
+]
 
-fake_histories1 = []
+fake_histories1 = [
+    ['INITIALIZE', 0],
+    ['CRYPT', 0],
+    ['CRYPT', 89],
+    ['PENDING', 108]
+]
 
-fake_histories2 = []
+fake_histories2 = [
+    ['INITIALIZE', 0],
+    ['CRYPT', 0],
+    ['PENDING', 20],
+    ['PENDING', 23],
+    ['DECRYPT', 23],
+    ['PROTECTED', 23]
+]
 
-fake_histories3 = []
+fake_histories3 = [
+    ['INITIALIZE', 0],
+]
 
-fake_histories4 = []
+fake_histories4 = [
+    ['INITIALIZE', 0],
+    ['CRYPT', 0],
+    ['CRYPT', 89],
+    ['PENDING', 108],
+    ['DECRYPT', 65],
+    ['DECRYPT', 108],
+    ['PROTECTED', 108]
+]
 
 fake_histories = {
     1: fake_histories1,
@@ -20,6 +47,8 @@ fake_histories = {
     3: fake_histories3,
     4: fake_histories4
 }
+
+
 
 
 def simulate_key(longueur=0):
